dd2b547be2b49d1e116c24218f127f4151123277
Bailey Poe <baileypoe1222@gmail.com>
2021-12-22 11:34:53 -0600

    added functionality for heroku deployment

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,17 +1,17 @@
-const { User, Book } = require('../models');
+const { Reader, Book } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
-// const { param } = require('../routes');
+
 
 const resolvers = {
 
     Query: {
 
-        //get a enjoyer by enjoyername
+        
         me: async (parent, args, context) => {
 
-            if (context.enjoyer) {
-                const info = await User.findOne({})
+            if (context.reader) {
+                const info = await Reader.findOne({})
                     .select('-__v -password')
                     .populate('books')
 
@@ -26,36 +26,36 @@ const resolvers = {
 
     Mutation: {
 
-        addUser: async (parent, args) => {
-            const enjoyer = await User.create(args);
-            const authToken = signToken(enjoyer);
+        userAddition: async (parent, args) => {
+            const reader = await Reader.create(args);
+            const authToken = signToken(reader);
 
-            return { authToken, enjoyer };
+            return { authToken, reader };
         },
 
         login: async (parent, { email, password }) => {
-            const enjoyer = await User.findOne({ email });
+            const reader = await Reader.findOne({ email });
 
-            if (!enjoyer) {
+            if (!reader) {
                 throw new AuthenticationError('Incorrect email or password');
             }
 
-            const correctPassword = await enjoyer.isCorrectPassword(password);
+            const correctPassword = await reader.isCorrectPassword(password);
 
             if (!correctPassword) {
                 throw new AuthenticationError('Incorrect email or password');
             }
 
-            const authToken = signToken(enjoyer);
-            return { authToken, enjoyer };
+            const authToken = signToken(reader);
+            return { authToken, reader };
 
         },
 
-        saveBook: async (parent, args, context) => {
-            if (context.enjoyer) {
+        bookSaving: async (parent, args, context) => {
+            if (context.reader) {
 
-                const userUpdate = await User.findByIdAndUpdate(
-                    { _id: context.enjoyer._id },
+                const userUpdate = await Reader.findByIdAndUpdate(
+                    { _id: context.reader._id },
                     { $addToSet: { savedBooks: args.input } },
                     { new: true }
                 );
@@ -68,10 +68,10 @@ const resolvers = {
 
 
 
-        removeBook: async (parent, args, context) => {
-            if (context.enjoyer) {
-                const userUpdate = await User.findOneAndUpdate(
-                    { _id: context.enjoyer._id },
+        bookDeletion: async (parent, args, context) => {
+            if (context.reader) {
+                const userUpdate = await Reader.findOneAndUpdate(
+                    { _id: context.reader._id },
                     { $pull: { savedBooks: { bookId: args.bookId } } },
                     { new: true }
                 );
